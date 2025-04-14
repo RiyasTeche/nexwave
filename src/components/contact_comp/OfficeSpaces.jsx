b65e862693e03b81d2659e340b22e4d621ae64e8
Riyas <m.riyas.muhammed@gmail.com>
2025-04-14 22:38:22 +0530

    conatct second pages finished

--- src/components/contact_comp/OfficeSpaces.jsx
+++ src/components/contact_comp/OfficeSpaces.jsx
@@ -5,55 +5,138 @@ const officeList = [
   {
     COUNTRY: "UGANDA",
     IMG: "uganda.png",
-    OFFICES: {
-      NAME: "nexwave shipping",
-      ADDRESS: "kampala",
-      CITY: "kampala",
-      PO: "671523",
-      COUNTRY: "Uganda",
-      OFFICEPHONE: "+9521833486",
-      MANAGER: "fasal",
-      PHONE: "+128345853",
-      SECONDPERSON: "Muhammed",
-      SECPHONE: "+128345853",
-      MAIL: "info@nexwave.com",
-    },
+    OFFICES: [
+      {
+        NAME: "nexwave shipping",
+        ADDRESS: "kampala",
+        CITY: "kampala",
+        PO: "671523",
+        COUNTRY: "Uganda",
+        OFFICEPHONE: "+9521833486",
+        FIRSTPERSON: "fasal",
+        ROLL: "Managing Director",
+        PHONE: "+128345853",
+        SECONDPERSON: "Muhammed",
+        SECPHONE: "+128345853",
+        MAIL: "info@nexwave.com",
+      },
+      {
+        NAME: "nexwave shipping",
+        ADDRESS: "kampala",
+        CITY: "jinja",
+        PO: "671523",
+        COUNTRY: "Uganda",
+        OFFICEPHONE: "+9521833486",
+        FIRSTPERSON: "fasal",
+        ROLL: "Managing Director",
+        PHONE: "+128345853",
+        SECONDPERSON: "Muhammed",
+        SECPHONE: "+128345853",
+        MAIL: "info@nexwave.com",
+      },
+    ],
   },
   {
     COUNTRY: "RWANDA",
     IMG: "rwanda.png",
-    OFFICES: {
-      NAME: "nexwave shipping",
-      ADDRESS: "kigali",
-      CITY: "kigali",
-      PO: "671523",
-      COUNTRY: "Rwanda",
-      OFFICEPHONE: "+952134563486",
-      MANAGER: "fasal",
-      PHONE: "+123235853",
-      SECONDPERSON: "Arun",
-      SECPHONE: "+1283455643",
-      MAIL: "info@nexwave.com",
-    },
+    OFFICES: [
+      {
+        NAME: "nexwave shipping",
+        ADDRESS: "kigali",
+        CITY: "kigali",
+        PO: "671523",
+        COUNTRY: "Rwanda",
+        OFFICEPHONE: "+952134563486",
+        MAIL: "info@nexwave.com",
+        FIRSTPERSON: "fasal",
+        ROLL: "Director",
+        PHONE: "+123235853",
+        SECONDPERSON: "Arun",
+        SECPHONE: "+1283455643",
+      },
+    ],
   },
   {
     COUNTRY: "UAE",
     IMG: "uae.jpg",
-    OFFICES: {
-      name: "nexwave LLC",
-      ADDRESS: "dubai",
-      CITY: "dubai",
-      PO: "671523",
-      COUNTRY: "UAE",
-      OFFICEPHONE: "+952134486",
-      MANAGER: "Salman",
-      PHONE: "+123235853",
-      SECONDPERSON: "asif",
-      SECPHONE: "+12455643",
-      MAIL: "info@nexwave.com",
-    },
+    OFFICES: [
+      {
+        name: "nexwave LLC",
+        ADDRESS: "dubai",
+        CITY: "dubai",
+        PO: "671523",
+        COUNTRY: "UAE",
+        OFFICEPHONE: "+952134486",
+        FIRSTPERSON: "Salman",
+        ROLL: "Manager",
+        PHONE: "+123235853",
+        SECONDPERSON: "asif",
+        SECPHONE: "+12455643",
+        MAIL: "info@nexwave.com",
+      },
+      {
+        name: "nexwave LLC",
+        ADDRESS: "dubai",
+        CITY: "abu dabi",
+        PO: "671523",
+        COUNTRY: "UAE",
+        OFFICEPHONE: "+952134486",
+        FIRSTPERSON: "Salman",
+        ROLL: "Manager",
+        PHONE: "+123235853",
+        SECONDPERSON: "asif",
+        SECPHONE: "+12455643",
+        MAIL: "info@nexwave.com",
+      },
+    ],
   },
 ];
+
+const OfficeInfo = ({ index }) => {
+
+  const offices = officeList[index].OFFICES;
+  console.log(offices);
+  return (
+    <>
+      {offices.map((item, i) => (
+        <div className="officeInfo" key={i}>
+          <h3>{item.CITY} OFFICE</h3>
+          <div className="officeInfoItem">
+            <div className="infoleft">
+              <ul>
+                <li>
+                  <p>{item.NAME}</p>
+                  <p>{item.ADDRESS}</p>
+                  <p>{item.PO}</p>
+                  <p>{item.CITY}</p>
+                  <p>{item.COUNTRY}</p>
+                </li>
+                <li>{item.OFFICEPHONE}</li>
+                <li>
+                  <p>{item.MAIL}</p>
+                </li>
+              </ul>
+            </div>
+            <div className="infoRight">
+              <ul>
+                <li>
+                  {item.FIRSTPERSON}
+                  <br />
+                  {item.ROLL} <br />
+                  <p>{item.PHONE}</p>
+                </li>
+                <li>
+                  <p>{item.SECONDPERSON}</p>
+                  <p>{item.SECPHONE}</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 const OfficeSpaces = () => {
   const [selectedIndex, setselectedIndex] = useState(0);
@@ -66,7 +149,7 @@ const OfficeSpaces = () => {
         </div>
         <div className="bottom">
           <div className="left">
-            <img src={"./"+ officeList[selectedIndex].IMG} alt="" />
+            <img src={"./" + officeList[selectedIndex].IMG} alt="" />
           </div>
           <div className="center">
             {officeList.map((item, i) => (
@@ -80,7 +163,9 @@ const OfficeSpaces = () => {
               </div>
             ))}
           </div>
-          <div className="right">{officeList[selectedIndex].OFFICES.SECONDPERSON}</div>
+          <div className="right">
+            <OfficeInfo index={selectedIndex} />
+          </div>
         </div>
       </div>
     </div>

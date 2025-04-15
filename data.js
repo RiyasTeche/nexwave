7f233ca500292c8bc20d4d5943908d4aa071a0e0
Riyas <m.riyas.muhammed@gmail.com>
2025-04-15 21:55:16 +0530

    Updates

---
+++ data.js
@@ -0,0 +1,174 @@
+export const company = [
+  {
+    name: "Nexwave Shipping & Logistics LLC",
+    type: "HeadOffice",
+    address: "Street Marco 45",
+    city: "London",
+    country: "UK",
+    phone: "+91 12345622",
+    mail: "info@nexwave.com",
+  },
+  {
+    name: "Nexwave Shipping & Logistics LLC",
+    type: "Region",
+    address: "#62 Road",
+    city: "Kigali",
+    country: "Rwanda",
+    phone: "+91 12345622",
+    mail: "info@nexwave.com",
+  }
+];
+
+export const servicesList = [
+  {
+    img: "lcl.jpg",
+    title: "LCL Shipping – Cost-Effective Global Freight",
+    desc: "LCL (Less than Container Load) shipping is the smart solution for smaller shipments that don't require a full container. Share container space and reduce costs while still enjoying reliable, secure, and timely delivery. Perfect for growing businesses and flexible logistics needs.",
+  },
+  {
+    img: "vessal.jpg",
+    title: "FCL Shipping",
+    desc: "(Full Container Load) Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+  },
+  {
+    img: "air.jpg",
+    title: "Air Freight – Fast & Reliable Delivery",
+    desc: "When speed matters, our air freight services deliver. Whether it’s urgent documents or high-value cargo, we ensure swift, secure, and on-time delivery across the globe. Count on us for priority handling and seamless logistics from takeoff to touchdown.",
+  },
+  {
+    img: "ship.jpg",
+    title: "Ocean Freight for Large Shipments",
+    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+  },
+  {
+    img: "doc.jpg",
+    title: "Customs Clearance & Documentation",
+    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+  },
+  {
+    img: "doortodoor.jpg",
+    title: "Door-to-Door Delivery",
+    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+  },
+  {
+    img: "wharehouse.jpg",
+    title: "Warehousing & Distribution",
+    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+  },
+  {
+    img: "consultant.jpg",
+    title: "Freight Forwarding & Logistics Consulting",
+    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+  },
+];
+
+export const officeList = [
+  {
+    COUNTRY: "UGANDA",
+    IMG: "uganda.png",
+    OFFICES: [
+      {
+        NAME: "nexwave shipping",
+        ADDRESS: "kampala",
+        CITY: "kampala",
+        PO: "671523",
+        COUNTRY: "Uganda",
+        OFFICEPHONE: "+9521833486",
+        FIRSTPERSON: "fasal",
+        ROLL: "Managing Director",
+        PHONE: "+128345853",
+        SECONDPERSON: "Muhammed",
+        SECPHONE: "+128345853",
+        MAIL: "info@nexwave.com",
+      },
+      {
+        NAME: "nexwave shipping",
+        ADDRESS: "kampala",
+        CITY: "jinja",
+        PO: "671523",
+        COUNTRY: "Uganda",
+        OFFICEPHONE: "+9521833486",
+        FIRSTPERSON: "fasal",
+        ROLL: "Managing Director",
+        PHONE: "+128345853",
+        SECONDPERSON: "Muhammed",
+        SECPHONE: "+128345853",
+        MAIL: "info@nexwave.com",
+      },
+    ],
+  },
+  {
+    COUNTRY: "RWANDA",
+    IMG: "rwanda.png",
+    OFFICES: [
+      {
+        NAME: "nexwave shipping",
+        ADDRESS: "kigali",
+        CITY: "kigali",
+        PO: "671523",
+        COUNTRY: "Rwanda",
+        OFFICEPHONE: "+952134563486",
+        MAIL: "info@nexwave.com",
+        FIRSTPERSON: "fasal",
+        ROLL: "Director",
+        PHONE: "+123235853",
+        SECONDPERSON: "Arun",
+        SECPHONE: "+1283455643",
+      },
+    ],
+  },
+  {
+    COUNTRY: "UAE",
+    IMG: "uae.png",
+    OFFICES: [
+      {
+        NAME: "nexwave LLC",
+        ADDRESS: "dubai",
+        CITY: "dubai",
+        PO: "671523",
+        COUNTRY: "UAE",
+        OFFICEPHONE: "+952134486",
+        FIRSTPERSON: "Salman",
+        ROLL: "Manager",
+        PHONE: "+123235853",
+        SECONDPERSON: "asif",
+        SECPHONE: "+12455643",
+        MAIL: "info@nexwave.com",
+      },
+      {
+        NAME: "Nexwave Abubhabi",
+        ADDRESS: "dubai",
+        CITY: "abu dabi",
+        PO: "671523",
+        COUNTRY: "UAE",
+        OFFICEPHONE: "+952134486",
+        FIRSTPERSON: "Salman",
+        ROLL: "Manager",
+        PHONE: "+123235853",
+        SECONDPERSON: "asif",
+        SECPHONE: "+12455643",
+        MAIL: "info@nexwave.com",
+      },
+    ],
+  },
+  {
+    COUNTRY: "India",
+    IMG: "india.png",
+    OFFICES: [
+      {
+        NAME: "Nexwave LLC Delhi",
+        ADDRESS: "delhi",
+        CITY: "delhi",
+        PO: "671523",
+        COUNTRY: "India",
+        OFFICEPHONE: "+95422134486",
+        FIRSTPERSON: "Riyas",
+        ROLL: "Director",
+        PHONE: "+123235853",
+        SECONDPERSON: "asif",
+        SECPHONE: "+12455643",
+        MAIL: "info@nexwave.com",
+      }
+    ],
+  },
+];

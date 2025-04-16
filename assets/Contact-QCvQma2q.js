ab5a521ee7d266048a09192fd6553a12f3ef3c91
Riyas <m.riyas.muhammed@gmail.com>
2025-04-16 19:11:57 +0530

    Updates

--- assets/Contact-QCvQma2q.js
+++ assets/Contact-QCvQma2q.js
@@ -1,4 +1,4 @@
-import{b as _g,g as kx,R as Sb,r as se,j as ze,m as Xx,o as Wm}from"./index-CkC92XaT.js";/**
+import{b as _g,g as kx,R as Sb,r as se,j as ze,m as Xx,o as Wm}from"./index-Bj0u3p09.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT

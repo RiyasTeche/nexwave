bb8e4d2688fd23fd9bb04a172b5399d15b9fa30e
Riyas <m.riyas.muhammed@gmail.com>
2025-06-01 22:13:39 +0530

    Updates

--- data.js
+++ data.js
@@ -22,43 +22,43 @@ export const company = [
 export const servicesList = [
   {
     img: "lcl.jpg",
-    title: "LCL Shipping – Cost-Effective Global Freight",
-    desc: "LCL (Less than Container Load) shipping is the smart solution for smaller shipments that don't require a full container. Share container space and reduce costs while still enjoying reliable, secure, and timely delivery. Perfect for growing businesses and flexible logistics needs.",
+    title: "LCL Shipping",
+    desc: "Cost-Effective Global Freight. LCL (Less than Container Load) shipping is the smart solution for smaller shipments that don't require a full container. Share container space and reduce costs while still enjoying reliable, secure, and timely delivery. Perfect for growing businesses and flexible logistics needs.",
   },
   {
     img: "vessal.jpg",
     title: "FCL Shipping",
-    desc: "(Full Container Load) Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+    desc: "Efficient, Secure, and Cost-Effective Full Container Load Solutions. Full Container Load (FCL) shipping refers to the transportation of goods that fill an entire container, either 20ft, 40ft, or 45ft. This method is ideal for businesses that have enough cargo to occupy the full space of a container or require dedicated space for their shipments.",
   },
   {
     img: "air.jpg",
-    title: "Air Freight – Fast & Reliable Delivery",
-    desc: "When speed matters, our air freight services deliver. Whether it’s urgent documents or high-value cargo, we ensure swift, secure, and on-time delivery across the globe. Count on us for priority handling and seamless logistics from takeoff to touchdown.",
+    title: "Air Freight",
+    desc: "Fast & Reliable Delivery. When speed matters, our air freight services deliver. Whether it’s urgent documents or high-value cargo, we ensure swift, secure, and on-time delivery across the globe. Count on us for priority handling and seamless logistics from takeoff to touchdown.",
   },
   {
     img: "ship.jpg",
-    title: "Ocean Freight for Large Shipments",
-    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+    title: "Ocean Freight",
+    desc: "Reliable, Global Sea Shipping Solutions Tailored to Your Business. Ocean freight is the most cost-effective method of transporting large volumes of goods across international waters. Whether you're shipping full containers (FCL) or smaller loads (LCL), ocean freight is a dependable solution for international trade.",
   },
   {
     img: "doc.jpg",
     title: "Customs Clearance & Documentation",
-    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+    desc: "Hassle-Free Clearance and Expert Documentation for Smooth Global Shipping. Customs clearance is a critical step in the shipping process. With constantly changing regulations, paperwork requirements, and compliance risks, it can quickly become overwhelming. That’s where we come in.Our experienced team ensures your shipments move smoothly across borders with minimal delays and full regulatory compliance.",
   },
   {
     img: "doortodoor.jpg",
     title: "Door-to-Door Delivery",
-    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+    desc: "Complete Shipping Solutions from Your Supplier’s Door to Yours. Our door-to-door delivery service handles the entire logistics chain,so you don’t have to. From pickup at the origin point to final delivery at the destination, we manage everything: transportation, customs clearance, documentation and coordination. Whether it’s across borders or across the globe, we ensure your cargo gets where it needs to be on time and in perfect condition.",
   },
   {
     img: "wharehouse.jpg",
     title: "Warehousing & Distribution",
-    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+    desc: "Secure Storage and Smart Logistics to Keep Your Supply Chain Moving. Our warehousing and distribution services give your business the flexibility, speed, and control it needs to thrive. Whether you need short-term storage, long-term inventory management, or fast order fulfillment, we’ve got the infrastructure and expertise to support you. From receiving and storing goods to picking, packing, and delivering them, we manage every step with precision.",
   },
   {
     img: "consultant.jpg",
-    title: "Freight Forwarding & Logistics Consulting",
-    desc: "Lorem ipsum dolotetur adipisicing elit. Quaerat illum porro pariatur neque, blanditiis reprehenderit. Ut exe vtis? Et quas placeat ratione assumenda.",
+    title: "Logistics Consulting",
+    desc: "Expert Guidance to Optimize Your Supply Chain and Boost Efficiency. In today’s fast-moving global market, logistics isn't just about moving goods-it's about moving them smarter. Our logistics consulting services help you streamline operations, cut unnecessary costs, and design a supply chain that works harder for your business. Whether you're launching a new product, expanding into new markets, or looking to improve performance, we provide the insights and solutions you need.",
   },
 ];
 
